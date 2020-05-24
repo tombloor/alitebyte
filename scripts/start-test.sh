e99bd7469852e780369db37663c1fab4b5812051
tombloor <5075314+tombloor@users.noreply.github.com>
2020-05-24 16:04:45 -0500

    switched to using dockerized builds

--- scripts/start-test.sh
+++ scripts/start-test.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
 
-bundle exec jekyll serve --drafts
+docker run \
+    --mount type=bind,source="$(pwd)",target=/app \
+    --rm -p 4000:4000 alitebyte \
+    bundle exec jekyll serve --drafts --host 0.0.0.0
+    
