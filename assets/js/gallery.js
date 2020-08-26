af805789543afdef6dea3bb1a21288221f1d184e
Tom Bloor <5075314+tombloor@users.noreply.github.com>
2020-08-26 10:43:07 -0500

    very simple image expander

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -6,4 +6,21 @@
 // scrolling through the set with arrows (eventually), wrap the .gallery-images
 // in a div with the class of .gallery-set
 
+// Maybe use bootstrap modals for the UI of this, seen as most of the 
+// work already done
+$(function(){
+    // Need to remove this and build it properly as a reusable
+    // component
+    let viewer = $('#gallery-viewer');
+    viewer.appendTo('#page');
 
+    $(document).on('click', '.gallery-image', function(){
+        let img = $(this).find('img').clone();
+        viewer.html(img);
+        viewer.removeClass('d-none');
+    });
+
+    $(document).on('click', '#gallery-viewer', function(){
+        viewer.addClass('d-none');
+    });
+});
